ec732e810e1a38e6d919420a214d7469cf30d29e
akmich <akmich@mail.ru>
2026-07-30 17:13:05 +0300

    исправление Checkstyle#3

--- src/ru/yandex/practicum/WordleGame.java
+++ src/ru/yandex/practicum/WordleGame.java
@@ -102,7 +102,8 @@ public class WordleGame {
 
             if (count > 0) {
                 currentResult[i] = LetterStatus.PRESENT;
-                answerCounts.put(c, (count-1));
+                int countToPut = count - 1;
+                answerCounts.put(c, countToPut);
                 presentLetters.add(c);
                 lettersOnWrongPlaces[currentStep][i] = c;
             } else {
