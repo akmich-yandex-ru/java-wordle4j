162dbf80ebe4b86663f8dad32ef3dc61cdac7fda
akmich <akmich@mail.ru>
2026-07-30 17:17:27 +0300

    исправление Checkstyle#5

--- src/exceptions/InvalidWordFormatException.java
+++ src/exceptions/InvalidWordFormatException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class InvalidWordFormatException extends RuntimeException{
+public class InvalidWordFormatException extends RuntimeException {
 
     public InvalidWordFormatException() {
         super("Введите слово из 5 русских букв");
